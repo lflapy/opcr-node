ad26fc42588cf07b6c429300a8ad30bc83b2b913
lflapy <lflapy@gmail.com>
2018-04-23 08:41:48 +0200

    2eme serveur.js le premier devient txt

--- serveur.js
+++ serveur.js
@@ -1,22 +1,20 @@
-
-// que fait donc ce code ??
-
 var http = require('http');
+
 var server = http.createServer(function(req, res) {
-  res.writeHead(200);
-  res.end('Salut tout le monde !');
+    res.writeHead(200, {"Content-Type": "text/html"});
+    res.write('<!DOCTYPE html>'+
+'<html>'+
+'    <head>'+
+'        <meta charset="utf-8" />'+
+'        <title>Ma page Node.js !</title>'+
+'    </head>'+
+'    <body>'+
+'     	<p>Voici un paragraphe <strong>HTML</strong> !</p>'+
+'     	<p>pas vraiment extra le <strong>HTML...</strong> !</p>'+
+'    </body>'+
+'</html>');
+    res.end();
 });
 server.listen(8080);
 
-
-// Il crée un mini-serveur web qui renvoie un message "Salut tout le monde" dans tous les cas,
-// quelle que soit la page demandée. Ce serveur est lancé sur le port 8080 à la dernière ligne.
-// Disséquons du code
-// Décomposons le code :
-// var http = require('http');   require effectue un appel à la bibliothèque.
-// Ici, la bibliothèque 'http' qui nous permet de créer un serveur web.
-// Il existe des tonnes de bibliothèque comme celle-là, la plupart pouvant être
-// téléchargées avec NPM, le gestionnaire de paquets de Node.js (on apprendra à
-// l'utiliser plus tard).
-// La variable http représente un objet JavaScript qui va nous permettre de lancer
-// un serveur web. C'est justement ce qu'on fait avec : var server = http.createServer();
+// conclusion : c'est pas extra l'écriture du html....
